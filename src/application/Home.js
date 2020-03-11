8d3e5bfc115199b7d8b5770ab8a432249d611a75
Gump-TUF <Gump1016@163.com>
2020-03-11 22:23:49 +0800

    顶部栏开发!

--- src/application/Home.js
+++ src/application/Home.js
@@ -1,4 +1,14 @@
 import React from 'react';
-import {renderRoutes} from "react-router-config";
-const Home = props => <div className="Home">{renderRoutes(props.route.routers)}</div>;
+import { renderRoutes } from 'react-router-config';
+import Top from './style';
+const Home = props => (
+  <div className="Home">
+      <Top>
+          <span className="iconfont menu">&#xe65c;</span>
+          <span className="title">WebApp</span>
+          <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      {renderRoutes(props.route.routers)}
+  </div>
+);
 export default React.memo(Home);
