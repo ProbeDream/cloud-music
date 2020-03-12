1252fc55ecc33e65e605ba362f1c8530c2559ff7
Gump-TUF <Gump1016@163.com>
2020-03-12 16:11:26 +0800

    实用redux提供的数据流!

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { IconStyle } from './assets/iconfont/iconfont';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter } from 'react-router-dom';
 import routes from './routes/index';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
 
 const App = () => (
-  <HashRouter>
-    <GlobalStyle></GlobalStyle>
-    <IconStyle></IconStyle>
-    {renderRoutes(routes)}
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      {renderRoutes(routes)}
+    </HashRouter>
+  </Provider>
 );
 
 export default App;
