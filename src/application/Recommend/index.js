6f9ebe244d6d41b0ed52e6af2dc1d031d8354f93
Gump-TUF <Gump1016@163.com>
2020-03-12 19:26:37 +0800

    将推荐组件改为了 函数表达式组件!

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import Slider from '../../components/Slider/';
 
-function Recommend() {
-  //mock 数据
-  const bannerList = [1, 2, 3, 4].map(item => {
-    return {
-      imageUrl:
-        'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg'
-    };
-  });
-
-  return (
-    <div>
-      <Slider bannerList={bannerList}></Slider>
-      Hello World!
-    </div>
-  );
-}
+const imageUrl = `http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg`;
+const bannerList = [1, 2, 3, 4].map(item => {
+  return {
+    imageUrl: imageUrl
+  };
+});
+const Recommend = () => (
+  <div>
+    <Slider bannerList={bannerList}></Slider>
+    Hello World!
+  </div>
+);
 
 export default React.memo(Recommend);
