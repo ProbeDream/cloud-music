7d441f55c7196671a4c2c043c08367d57b47993d
Gump-TUF <Gump1016@163.com>
2020-03-12 16:05:58 +0800

    全局样式 格式化!

--- src/assets/global-style.js
+++ src/assets/global-style.js
@@ -1,20 +1,20 @@
 const extendClick = () => {
   return `
-    position:relative;
-    &:before{
-        content:'';
-        position:absolute;
-        top:-10px;bottom:-10px;left:-10px;right:-10px;
-    }
-    `;
+    position: relative;
+    &:before {
+      content: '';
+      position: absolute;
+      top: -10px; bottom: -10px; left: -10px; right: -10px;
+    };
+  `;
 };
-
+// 一行文字溢出部分用... 代替
 const noWrap = () => {
   return `
-    text-overflow:ellipsis;
-    overflow:hidden;
-    white-space:nowrap;
-    `;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  `;
 };
 
 export default {
