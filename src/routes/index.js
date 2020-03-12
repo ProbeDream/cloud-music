8a0eab99b06b8accb4c9392cf2d151a1a4230240
Gump-TUF <Gump1016@163.com>
2020-03-12 16:07:01 +0800

    路由表的redirect重定向组件应该是来自react-router-dom!

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import Home from '../application/Home';
 import Recommend from '../application/Recommend';
 import Singers from '../application/Singers';
